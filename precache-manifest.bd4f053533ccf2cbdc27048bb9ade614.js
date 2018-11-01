53f2d94f9ce643e568805d5ad3385129f2908abf
J. Graves <jwgravesfl@gmail.com>
2018-10-31 22:22:23 -0600

    Updates

---
+++ precache-manifest.bd4f053533ccf2cbdc27048bb9ade614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c63753d45ca1398fae29",
+    "url": "/thespotsocial/static/js/runtime~main.c63753d4.js"
+  },
+  {
+    "revision": "d04f09fa0d28a59ad390",
+    "url": "/thespotsocial/static/js/main.d04f09fa.chunk.js"
+  },
+  {
+    "revision": "24fedbd08693f60c9623",
+    "url": "/thespotsocial/static/js/1.24fedbd0.chunk.js"
+  },
+  {
+    "revision": "24fedbd08693f60c9623",
+    "url": "/thespotsocial/static/css/1.9cfaa699.chunk.css"
+  },
+  {
+    "revision": "6668e3cb9e04c57fdd73c3936f02b538",
+    "url": "/thespotsocial/index.html"
+  }
+];
